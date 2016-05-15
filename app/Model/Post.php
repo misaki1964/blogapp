c96eaf25a2e3cbe80442a691f9429bfe7ccf40a1
Motohide Misaki <misaki1964@gmail.com>
2016-05-15 06:32:37 +0000

    Postモデルを追加

---
+++ app/Model/Post.php
@@ -0,0 +1,41 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Post Model
+ *
+ */
+class Post extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'title' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				'message' => 'タイトルは必須入力です',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'maxLength' => array(
+				'rule' => ['maxLength', '255'],
+				'message' => 'タイトルは255文字以内で入力してください',
+			),
+
+		),
+		'body' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
