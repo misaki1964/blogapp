90827d130fa4015c9e96b3fa8968cdd1be44cefb
Motohide Misaki <misaki1964@gmail.com>
2016-05-29 08:22:10 +0000

    テストデータは Fabricate を使って生成するように変更

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -11,19 +11,4 @@ class PostFixture extends CakeTestFixture {
  */
 	public $import = array('model' => 'Post');
 
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id' => 1,
-			'title' => 'Lorem ipsum dolor sit amet',
-			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-			'created' => '2016-05-22 07:45:58',
-			'modified' => '2016-05-22 07:45:58'
-		),
-	);
-
 }
