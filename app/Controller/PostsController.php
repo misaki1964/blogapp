45fd4f8f9d15690848c6d8f46e424a7999dd3a39
Motohide Misaki <misaki1964@gmail.com>
2016-06-05 03:06:29 +0000

    ビューを追加

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -14,6 +14,14 @@ class PostsController extends AppController {
 
 	public $components = ['Paginator', 'RequestHandler', 'Session'];
 
+// $helpers を追加 ここから
+	public $helpers = [
+		'Html' => ['className' => 'BoostCake.BoostCakeHtml'],
+		'Form' => ['className' => 'BoostCake.BoostCakeForm'],
+	];
+// ここまで
+
+
 	public function index() {
 		$this->set('posts', $this->Paginator->paginate());
 	}
@@ -22,15 +30,22 @@ class PostsController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Post->create($this->request->data);
 			if ($this->Post->save()) {
+// メッセージの出力部分に boostrap のパラメータを追加
 				$this->Session->setFlash(
-					__('新しい記事を受け付けました。')
+					__('新しい記事を受け付けました。'),
+					'alert',
+					['plugin' => 'BoostCake', 'class' => 'alert-success']
 				);
-
+// ここまで
 				return $this->redirect(['action' => 'index']);
 			} else {
+// メッセージの出力部分に boostrap のパラメータを追加
 				$this->Session->setFlash(
-					__('記事の投稿に失敗しました。入力内容を確認して再度投稿してください。')
+					__('記事の投稿に失敗しました。入力内容を確認して再度投稿してください。'),
+					'alert',
+					['plugin' => 'BoostCake', 'class' => 'alert-danger']
 				);
+// ここまで
 			}
 		}
 	}
